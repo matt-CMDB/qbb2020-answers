de2c71904d0e16afcd792c303e85d9ef5af1bf97
Matt Lubin <mlubin4@jhu.edu>
2020-12-18 14:35:25 -0500

    kind of almost basically complete

--- qbl-week2/bwa_align.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for SAMPLE in 09 11 23 24 27 31 35 39 62 62
-do
-  bwa mem -t 4 -R "@RG\tID:${SAMPLE}\tSM:${SAMPLE}" -o ${SAMPLE}.sam sacCer3.fa A01_${SAMPLE}.fastq
-  samtools sort -o ${SAMPLE}_sorted.bam ${SAMPLE}.sam
-done
